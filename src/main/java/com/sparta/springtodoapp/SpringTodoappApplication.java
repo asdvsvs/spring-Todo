1d06e334a19bd69098de009753e6a128f640bd3a
asdvsvs <gag973000@gmail.com>
2023-11-15 20:39:42 +0900

    init commit

---
+++ src/main/java/com/sparta/springtodoapp/SpringTodoappApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springtodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTodoappApplication.class, args);
+	}
+
+}
