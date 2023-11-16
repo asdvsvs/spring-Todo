cb3d90433d2dc9cf6b4fdb617c014e83b741a031
asdvsvs <gag973000@gmail.com>
2023-11-16 19:11:02 +0900

    feat : 로그인 기능 추가

--- src/main/java/com/sparta/springtodoapp/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/springtodoapp/config/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,6 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity // Spring Security 지원을 가능하게 함
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
     private final JwtUtil jwtUtil;
     private final UserDetailsServiceImpl userDetailsService;
@@ -50,22 +52,14 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/api/signup").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/api/**").permitAll() // '/api/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
         // 로그인 사용
         http.formLogin((formLogin) ->
                 formLogin
-                        // 로그인 View 제공 (GET /api/user/login-page)
-                        .loginPage("/api/user/login-page")
-                        // 로그인 처리 (POST /api/user/login)
-                        .loginProcessingUrl("/api/user/login")
-                        // 로그인 처리 후 성공 시 URL
-                        .defaultSuccessUrl("/")
-                        // 로그인 처리 후 실패 시 URL
-                        .failureUrl("/api/user/login-page?error")
-                        .permitAll()
+                        .loginPage("/api/user/login-page").permitAll()
         );
 
         // 필터 관리
