bf4c0b1682d6a37152dcb1f948979d7f9093b866
asdvsvs <gag973000@gmail.com>
2023-12-02 21:48:37 +0900

    test : CommentService 테스트 코드 커버리지 향상

--- src/test/java/com/sparta/springtodoapp/service/CommentServiceTest.java
+++ src/test/java/com/sparta/springtodoapp/service/CommentServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Optional;
 
@@ -35,16 +36,24 @@ class CommentServiceTest {
         //given
         Users user = new Users("sparta", "12345678");
         UserDetailsImpl userDetails = new UserDetailsImpl(user);
-        CommentRequestDto requestDto = new CommentRequestDto("coding",user.getUsername(),"club","game");
+        CommentRequestDto requestDto = new CommentRequestDto("coding", user.getUsername(), "club", "game");
         CommentService commentService = new CommentService(commentRepository, todoRepository, userRepository);
         given(userRepository.findByUsername(user.getUsername())).willReturn(Optional.of(user));
-        given(todoRepository.findByTitleAndUsersId(requestDto.getTitle(),user.getId())).willReturn(new Todo());
+        given(todoRepository.findByTitleAndUsersId(requestDto.getTitle(), user.getId())).willReturn(new Todo());
 
-        //when
-        CommentResponseDto responseDto =commentService.writeComment(userDetails, requestDto);
+        //when -then
 
-        //then
-        assertEquals(requestDto.getContent(),responseDto.getContent());
+        CommentResponseDto responseDto = commentService.writeComment(userDetails, requestDto);
+
+        assertThrows(IllegalArgumentException.class, () -> commentService.writeComment(null, requestDto));
+
+        when(userRepository.findByUsername(userDetails.getUsername())).thenReturn(Optional.empty());
+        assertThrows(UsernameNotFoundException.class, () -> commentService.writeComment(userDetails, requestDto));
+        assertEquals(requestDto.getContent(), responseDto.getContent());
+
+        given(userRepository.findByUsername(user.getUsername())).willReturn(Optional.of(user));
+        when(todoRepository.findByTitleAndUsersId(requestDto.getTitle(), user.getId())).thenReturn(null);
+        assertThrows(IllegalArgumentException.class, () -> commentService.writeComment(userDetails, requestDto));
     }
 
     @Test
@@ -52,18 +61,18 @@ class CommentServiceTest {
         //given
         Users user = new Users("sparta", "12345678");
         UserDetailsImpl userDetails = new UserDetailsImpl(user);
-        Comment comment = new Comment("beforeUpdate",user,new Todo());
-        CommentRequestDto requestDto = new CommentRequestDto("coding",user.getUsername(),"beforeUpdate","AfterUpdate");
+        Comment comment = new Comment("beforeUpdate", user, new Todo());
+        CommentRequestDto requestDto = new CommentRequestDto("coding", user.getUsername(), "beforeUpdate", "AfterUpdate");
         CommentService commentService = new CommentService(commentRepository, todoRepository, userRepository);
         given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(user));
-        given(todoRepository.findByTitleAndUsersId(requestDto.getTitle(),user.getId())).willReturn(new Todo());
-        given(commentRepository.findByContentAndUsersId(requestDto.getContent(),user.getId())).willReturn(comment);
+        given(todoRepository.findByTitleAndUsersId(requestDto.getTitle(), user.getId())).willReturn(new Todo());
+        given(commentRepository.findByContentAndUsersId(requestDto.getContent(), user.getId())).willReturn(comment);
 
         //when - then
-        assertNotEquals(comment.getContent(),requestDto.getUpdateContent());
+        assertNotEquals(comment.getContent(), requestDto.getUpdateContent());
 
-        commentService.updateComment(userDetails,requestDto);
-        assertEquals(comment.getContent(),requestDto.getUpdateContent());
+        commentService.updateComment(userDetails, requestDto);
+        assertEquals(comment.getContent(), requestDto.getUpdateContent());
 
     }
 
@@ -72,20 +81,38 @@ class CommentServiceTest {
         //given
         Users user = new Users("sparta", "12345678");
         UserDetailsImpl userDetails = new UserDetailsImpl(user);
-        Comment comment = new Comment("beforeUpdate",user,new Todo());
-        CommentRequestDto requestDto = new CommentRequestDto("coding",user.getUsername(),"beforeUpdate","AfterUpdate");
+        Comment comment = new Comment("beforeUpdate", user, new Todo());
+        CommentRequestDto requestDto = new CommentRequestDto("coding", user.getUsername(), "beforeUpdate", "AfterUpdate");
         CommentService commentService = new CommentService(commentRepository, todoRepository, userRepository);
         given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(user));
-        given(todoRepository.findByTitleAndUsersId(requestDto.getTitle(),user.getId())).willReturn(new Todo());
-        given(commentRepository.findByContentAndUsersId(requestDto.getContent(),user.getId())).willReturn(comment);
+        given(todoRepository.findByTitleAndUsersId(requestDto.getTitle(), user.getId())).willReturn(new Todo());
+        given(commentRepository.findByContentAndUsersId(requestDto.getContent(), user.getId())).willReturn(comment);
 
         //when - then
-        commentService.deleteComment(userDetails,requestDto);
-        given(commentRepository.findByContentAndUsersId(requestDto.getContent(),user.getId())).willReturn(null);
+        commentService.deleteComment(userDetails, requestDto);
+        given(commentRepository.findByContentAndUsersId(requestDto.getContent(), user.getId())).willReturn(null);
         NullPointerException exception = assertThrows(
                 NullPointerException.class,
-                ()->commentService.deleteComment(userDetails,requestDto)
+                () -> commentService.deleteComment(userDetails, requestDto)
         );
-        assertEquals("해당 댓글은 존재하지 않습니다.",exception.getMessage());
+        assertEquals("해당 댓글은 존재하지 않습니다.", exception.getMessage());
+
+        assertThrows(IllegalArgumentException.class, () -> commentService.deleteComment(null, requestDto));
+
+        when(userRepository.findByUsername(userDetails.getUsername())).thenReturn(Optional.empty());
+        assertThrows(UsernameNotFoundException.class, () -> commentService.deleteComment(userDetails, requestDto));
+
+        given(userRepository.findByUsername(user.getUsername())).willReturn(Optional.of(user));
+        when(todoRepository.findByTitleAndUsersId(requestDto.getTitle(), user.getId())).thenReturn(null);
+        assertThrows(NullPointerException.class, () -> commentService.deleteComment(userDetails, requestDto));
+
+        given(userRepository.findByUsername(user.getUsername())).willReturn(Optional.of(user));
+        given(commentRepository.findByContentAndUsersId(requestDto.getContent(), user.getId())).willReturn(comment);
+        when(todoRepository.findByTitleAndUsersId(requestDto.getTitle(), user.getId())).thenReturn(new Todo());
+        assertThrows(IllegalArgumentException.class, () -> commentService.deleteComment(
+                new UserDetailsImpl(new Users("스파르타","12345678")),
+                requestDto
+                )
+        );
     }
 }
