34892638c7e4755a8d493b1ef7197005b39bf32e
asdvsvs <gag973000@gmail.com>
2023-12-04 20:02:54 +0900

    fix : UsersServiceTest.java - CommentTest 인터페이스 상속, @Nested 애노테이션 사용해서 성공,실패 케이스 그룹화

--- src/test/java/com/sparta/springtodoapp/service/UsersServiceTest.java
+++ src/test/java/com/sparta/springtodoapp/service/UsersServiceTest.java
@@ -3,6 +3,9 @@ package com.sparta.springtodoapp.service;
 import com.sparta.springtodoapp.dto.UserRequestDto;
 import com.sparta.springtodoapp.entity.Users;
 import com.sparta.springtodoapp.repository.UserRepository;
+import com.sparta.springtodoapp.test.CommonTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -13,61 +16,115 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class UsersServiceTest {
+class UsersServiceTest implements CommonTest {
 
     @Mock
     UserRepository userRepository;
     @Mock
     PasswordEncoder passwordEncoder;
 
-    @Test
-    void signup() {
-        //given
-        UserRequestDto requestDto = new UserRequestDto("sparta", "codingClub");
-        Users users = new Users(requestDto.getUsername(), passwordEncoder.encode(requestDto.getPassword()));
-        UserService userService = new UserService(userRepository, passwordEncoder);
+    @Nested
+    @DisplayName("회원가입")
+    class signup{
+        @Test
+        @DisplayName("회원가입 성공")
+        void signup_success() {
+            //given
+            UserRequestDto requestDto = new UserRequestDto(TEST_USER_NAME, TEST_USER_PASSWORD);
+            Users users = new Users(requestDto.getUsername(), passwordEncoder.encode(requestDto.getPassword()));
+            UserService userService = new UserService(userRepository, passwordEncoder);
+            given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.empty());
 
-        //when
-        given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(users));
-        IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class,
-                () -> userService.signup(requestDto)
-        );
+            //when
+            userService.signup(requestDto);
 
-        given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.empty());
-        userService.signup(requestDto);
+            //then
 
-        //then
-        assertEquals("중복된 사용자 이름입니다", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("회원가입 실패")
+        void signup_fail() {
+            //given
+            UserRequestDto requestDto = new UserRequestDto(TEST_USER_NAME, TEST_USER_PASSWORD);
+            Users users = new Users(requestDto.getUsername(), passwordEncoder.encode(requestDto.getPassword()));
+            UserService userService = new UserService(userRepository, passwordEncoder);
+            given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(users));
+
+            //when
+            IllegalArgumentException exception = assertThrows(
+                    IllegalArgumentException.class,
+                    () -> userService.signup(requestDto)
+            );
+
+            //then
+            assertEquals("중복된 사용자 이름입니다", exception.getMessage());
+        }
+
     }
 
-    @Test
-    void login() {
-        //given
-        UserRequestDto requestDto = new UserRequestDto("sparta", "codingClub");
-        Users users = new Users(requestDto.getUsername(), passwordEncoder.encode(requestDto.getPassword()));
-        UserService userService = new UserService(userRepository, passwordEncoder);
+    @Nested
+    @DisplayName("로그인")
+    class login{
+        @Test
+        @DisplayName("로그인 성공")
+        void login_success() {
+            //given
+            UserRequestDto requestDto = new UserRequestDto(TEST_USER_NAME, TEST_USER_PASSWORD);
+            Users users = new Users(requestDto.getUsername(), passwordEncoder.encode(requestDto.getPassword()));
+            UserService userService = new UserService(userRepository, passwordEncoder);
+            given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(users));
+            given(passwordEncoder.matches(requestDto.getPassword(), users.getPassword())).willReturn(true);
 
-        //when
-        given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.empty());
-        NullPointerException nullPointerException = assertThrows(
-                NullPointerException.class,
-                ()-> userService.login(requestDto)
-        );
-        given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(users));
-        given(passwordEncoder.matches(requestDto.getPassword(), users.getPassword())).willReturn(false);
-        IllegalArgumentException illegalArgumentException = assertThrows(
-                IllegalArgumentException.class,
-                ()-> userService.login(requestDto)
-        );
-        given(passwordEncoder.matches(requestDto.getPassword(), users.getPassword())).willReturn(true);
-        userService.login(requestDto);
+            //when
+            userService.login(requestDto);
 
+            //then
 
-        //then
-        assertEquals("등록된 유저가 없습니다.",nullPointerException.getMessage());
-        assertEquals("비밀번호가 일치하지 않습니다.",illegalArgumentException.getMessage());
+        }
+
+        @Test
+        @DisplayName("로그인 실패 - 존재하지 않는 유저")
+        void login_fail_suchUser() {
+            //given
+            UserRequestDto requestDto = new UserRequestDto(TEST_USER_NAME, TEST_USER_PASSWORD);
+            Users users = new Users(requestDto.getUsername(), passwordEncoder.encode(requestDto.getPassword()));
+            UserService userService = new UserService(userRepository, passwordEncoder);
+            given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.empty());
+
+            //when
+            NullPointerException nullPointerException = assertThrows(
+                    NullPointerException.class,
+                    ()-> userService.login(requestDto)
+            );
+
+            //then
+            assertEquals("등록된 유저가 없습니다.",nullPointerException.getMessage());
+
+        }
+
+        @Test
+        @DisplayName("로그인 실패 - 비밀번호 불일치")
+        void login_fail_password() {
+            //given
+            UserRequestDto requestDto = new UserRequestDto(TEST_USER_NAME, TEST_USER_PASSWORD);
+            Users users = new Users(requestDto.getUsername(), passwordEncoder.encode(requestDto.getPassword()));
+            UserService userService = new UserService(userRepository, passwordEncoder);
+            given(userRepository.findByUsername(requestDto.getUsername())).willReturn(Optional.of(users));
+            given(passwordEncoder.matches(requestDto.getPassword(), users.getPassword())).willReturn(false);
+
+            //when
+            IllegalArgumentException illegalArgumentException = assertThrows(
+                    IllegalArgumentException.class,
+                    ()-> userService.login(requestDto)
+            );
+
+            //then
+            assertEquals("비밀번호가 일치하지 않습니다.",illegalArgumentException.getMessage());
+        }
+
     }
 }
